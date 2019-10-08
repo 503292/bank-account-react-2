5c83e517c6d189d65e2c7de891d2137a2b8a9761
Kalysh <503292@ukr.net>
2019-10-08 18:06:48 +0300

    Updates

---
+++ precache-manifest.f04fc2489c8aaafb8c313631c682749c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5ee3ba7686b19c3e22c1b4cce6362e",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "d2662b17318e9f47d250",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "3178d85880ae9b09fab1",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.04bb6114.chunk.css"
+  },
+  {
+    "revision": "d2662b17318e9f47d250",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.f0617b6e.chunk.js"
+  },
+  {
+    "revision": "3178d85880ae9b09fab1",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.40a2ab60.chunk.js"
+  },
+  {
+    "revision": "47c05aba7d6a1e74c8fd",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.64331d6b.js"
+  }
+]);
